b834ada2fa18e3d0971626771ffa35f77664e2eb
Byroni <bchrispark@gmail.com>
2017-01-18 02:06:54 -0500

    Basic movement and mouse look with crosshair

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 /* Goes on the player object */
 public class MovementController : MonoBehaviour {
 	public Rigidbody playerBody;
-	public Camera playerCam;
-	public float moveSpeed;
+	public CharacterController cc;
+
+	public float moveSpeed = 10.0f;
 	public float jumpForce;
+
+	public float mouseSensitivity = 5.0f;
+
+	public float verticalRotation = 0;
+	public float upDownRange = 60.0f;
 
 	void Start () {
 		playerBody = this.GetComponent<Rigidbody> ();
+		cc = GetComponent<CharacterController> ();
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	void Update () {
-		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed;
-		float z = Input.GetAxis ("Vertical") * Time.deltaTime * moveSpeed;
+		/* Rotation */
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * mouseSensitivity , 0);
 
-		Vector3 mousePos = Input.mousePosition;
-		float mousePosToPlayer = Input.mousePosition.x - transform.position.x;
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp (verticalRotation, -upDownRange, upDownRange);
 
-		transform.LookAt (playerCam.ScreenToWorldPoint (mousePos), Vector3.up); 
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0); 
 
-		transform.Translate (x, 0, z);
+		/* Movement */
+		float sideMovement = Input.GetAxis ("Horizontal") * moveSpeed;
+		float fowardMovement = Input.GetAxis ("Vertical") * moveSpeed;
+
+		Vector3 speed = new Vector3 (sideMovement, 0, fowardMovement);
+
+		speed = transform.rotation * speed;
+		cc.SimpleMove (speed);
 	}
 }
