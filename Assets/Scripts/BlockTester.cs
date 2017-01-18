b0e0393567a9a22a7a3d60b862b888eecb11f429
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-01-18 11:49:12 -0500

    Added block model and testing scene
    * Block and Tower Generation
    
    First implementation of the blocks, including free asset texture applied. Tower can stand on its own
    
    * Added Script for basic testing
    
    Added a tester script to the scene so we can shoot pieces around. As of this commit things look like they are working well.
    
    * Improved Camera Angle
    
    Improved camera angle to showcase the current status better
    
    * Added block model object
    
    Adding the model object for the block in the correct folder. I stil have to make sure that the assets folders get destroyed.
    
    * Made some organizational changes
    
    Renaming files and moving things around

---
+++ Assets/Scripts/BlockTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTester : MonoBehaviour {
+
+	public Vector3 forceToApply;
+	public GameObject objectToMove;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			print ("Applying the force for test");
+			objectToMove.GetComponent<Rigidbody> ().AddForce (forceToApply);
+		}
+	}
+}
