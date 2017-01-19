78bbcaa4b2dd8b8f8a15461efc69499a4201d4c0
Byroni <bchrispark@gmail.com>
2017-01-19 14:49:24 -0500

    Working on shooting mechanic

---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+	public GameObject arrowPrefab;
+	private GameObject arrowClone;
+	public Transform firePoint;
+	public float shootForce = 500.0f;
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			arrowClone = Instantiate (arrowPrefab, firePoint.position, Quaternion.identity);
+			arrowClone.GetComponent<Rigidbody> ().isKinematic = false;
+			arrowClone.GetComponent<Rigidbody> ().AddForce (Vector3.forward * shootForce);
+		}
+	}
+}
