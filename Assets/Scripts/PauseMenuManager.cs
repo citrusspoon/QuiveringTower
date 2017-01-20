af5b45cc72fc87049257fe8dc0254346993effa3
Byroni <bchrispark@gmail.com>
2017-01-20 16:37:44 -0500

    Created PauseManager script and capitalized function names for naming conventions

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour {
+
+	/* Close the pause menu */
+	public void CloseMenu () {
+		this.GetComponent<Canvas> ().enabled = false;
+	}
+
+	public void OpenSettings () {
+		//	Open the settings menu
+	}
+
+	public void ExitGame () {
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Menu");
+	}
+
+}
