077a0f6d212548a4af86abef7af67fa6a20fc53a
Scott Schreiber <sschrei1@uncc.edu>
2017-01-21 18:06:08 -0500

    Dissolve arrow now only destroys blocks.

--- Assets/Scripts/DissolveArrowController.cs
+++ Assets/Scripts/DissolveArrowController.cs
@@ -9,11 +9,14 @@ public class DissolveArrowController : MonoBehaviour {
 		ArrowStick (col);
 	}
 
-	void ArrowStick (Collision col) {
-		arrow.isKinematic = true;	//	Stop the arrow
-	
-		transform.parent = col.transform;	//	Make the arrow a child of the gameobject it hits
-        Destroy(col.gameObject);
+    void ArrowStick(Collision col) {
+        arrow.isKinematic = true;   //	Stop the arrow
 
+        transform.parent = col.transform;   //	Make the arrow a child of the gameobject it hits
+
+        if (col.gameObject.name == "block" || col.gameObject.name == "block (1)" || col.gameObject.name == "block (2)") { 
+
+            Destroy(col.gameObject);
+        }
 	}
 }
