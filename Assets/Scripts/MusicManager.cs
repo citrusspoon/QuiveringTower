0ef3b96f167b6890f3b26c41f3cd2c75c692bd05
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-01-24 10:49:58 -0500

    Created music player prefab
    Created music player prefab (singleton). Just need to throw in some music.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private static MusicManager singleMusicManager;
+
+	// Using Load function to use a singleton pattern
+	void Awake () {
+		if (singleMusicManager == null) {
+			DontDestroyOnLoad (this.gameObject);
+			singleMusicManager = this;
+		} else {
+			GameObject.Destroy (this.gameObject);
+		}
+	}
+
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+}
