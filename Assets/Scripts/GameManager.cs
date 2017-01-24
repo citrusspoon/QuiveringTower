129b0da1afa279a7bfc3f88a71ff7b0cfe366ca1
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-01-24 12:36:42 -0500

    Created In-game UI / Merge pull request #53 from Bananapen/Ingame-UI
    * Created basic in game UI design
    
    * created prefab for the ingame UI
    
    * Improved scaling of the UI for different resolutions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+	public Text timerText;
+	public Image timerFill;
+
+	private float turnTimeLeft;
+	public float totalTurnTime;
+
 	// Use this for initialization
 	void Start () {
-		
+		turnTimeLeft = totalTurnTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		turnTimeLeft -= Time.deltaTime;
+		timerText.text = Mathf.RoundToInt (turnTimeLeft).ToString();
+
+		//print (turnTimeLeft / totalTurnTime);
+		timerFill.fillAmount = turnTimeLeft / totalTurnTime;
 	}
 }
