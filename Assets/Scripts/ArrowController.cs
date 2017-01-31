02d68736f64b4f1c02953ee9810b79c3fed8c089
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-01-31 16:47:33 -0500

    Fixed raycast algorithm
    Decreased raycast sticking distance

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -11,33 +11,37 @@ public class ArrowController : MonoBehaviour {
 
 	private Ray movementRay;
 	private RaycastHit raycastResult;
+	private Transform arrowWillStick = null;
 
 	void Start ()
 	{
 		movementRay = new Ray ();
 		lastOrientation = transform.rotation; // Store initial orientation of the arrow
 	}
-//
-//	void OnCollisionEnter (Collision col) {	//	When the arrow hits something
-//		ArrowStick (col);
-//	}
-//
-//	void ArrowStick (Collision col) {
-//		arrow.isKinematic = true;	//	Stop the arrow
-//		arrow.detectCollisions = false;
-//		transform.parent = col.transform;	//	Make the arrow a child of the gameobject it hits
-//
-//	}
 
-	void FixedUpdate(){
-		if (arrow.velocity.magnitude>0.1) {		// If the arrow is flying, orient it with the velocity
+	void FixedUpdate ()
+	{
+
+		movementRay.origin = transform.position;
+		movementRay.direction = arrow.velocity;
+
+
+		if (arrowWillStick != null) {
+				//Stop the arrow
+				arrow.isKinematic = true;
+
+				// Stick to the surface shot
+				transform.parent = arrowWillStick;
+		}
+
+		if (arrow.velocity.magnitude > 0.1) {		// If the arrow is flying, orient it with the velocity
 			//TODO Optimize this script by moving the GetComp.
 			transform.rotation = Quaternion.LookRotation (arrow.velocity) * Quaternion.Euler (Vector3.right * 90);
 			lastOrientation = transform.rotation;
 
-
+			//Debug.Break();
 			// Test for possible hits in the next frame
-			if (Physics.Raycast(movementRay,out raycastResult,GetComponent<Rigidbody> ().velocity.magnitude)){
+			if (Physics.Raycast (movementRay, out raycastResult, GetComponent<Rigidbody> ().velocity.magnitude * 0.02f)) {
 
 				if (raycastResult.rigidbody != null) {
 					//Arrow effects
@@ -55,24 +59,22 @@ public class ArrowController : MonoBehaviour {
 						break;
 					}
 				}
+
+				arrowWillStick = raycastResult.transform;
 					
 				//Stop the arrow
 				arrow.isKinematic = true;
 
 				// Stick to the surface shot
 				transform.position = raycastResult.point;
-				transform.parent = raycastResult.transform;
+				GetComponent<Rigidbody> ().MovePosition (raycastResult.point);
+				transform.SetParent(raycastResult.transform);
 			}
 
 		} else {
 			transform.rotation = lastOrientation;	// If the arrow stops retain last known orientation
 		}
+
 	}
 
-
-	void Update(){
-		// Setup ray for raycasting
-		movementRay.origin = transform.position;
-		movementRay.direction = arrow.velocity;
-	}
 }
