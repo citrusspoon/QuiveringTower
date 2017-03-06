d44d42ec36d36207673061e8516bc5209a14da7f
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-03-06 09:36:07 -0500

    Basic point system

---
+++ Assets/BlockController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+	public bool isTouchingGround = false;
+	
+	// Keep reference to base blocks so other blocks can rest on them
+	[SerializeField] private bool isBaseBlock = false;
+	public void OnCollisionStay(Collision collision){
+		// If the object we are touching is tagged as ground this block should also be grounded
+		if (!isBaseBlock && collision.gameObject.tag == "Ground" && !isTouchingGround){
+			isTouchingGround = true;
+			gameObject.tag = "Ground";		
+			RulesManager.manager.blockFallen();
+			}
+	}
+
+}
---
+++ Assets/Scripts/RulesManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesManager : MonoBehaviour {
+
+	public int playerScore = 0;
+
+	public static RulesManager manager = null;
+
+	private bool blockDownThisTurn = false;
+	// Use this for initialization
+	void Start () {
+		if (manager == null){
+			RulesManager.manager = this;
+		} else {
+			Destroy(this);
+		}
+	}
+	
+	public void blockFallen(){
+		if (!blockDownThisTurn){
+			playerScore += 50;
+			blockDownThisTurn = true;
+		} else {
+			playerScore -= 10;
+		}
+		print (playerScore);
+	}
+}
