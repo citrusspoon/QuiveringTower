a064caa722d9a815384e3dd32fe728ebfa7a8864
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2017-03-22 09:56:23 -0400

    Improved Main Menu Setup

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+	[SerializeField] private Button SinglePlayerFreePlay, SinglePlayerChallanges;
+
+	void Start(){
+		SinglePlayerChallanges.onClick.AddListener(StartSinglePlayerChallanges);
+		SinglePlayerFreePlay.onClick.AddListener(StartSinglePlayerFreePlay);
+	}
+
+	void StartSinglePlayerChallanges(){
+		LevelManager.manager.StartNewGame(LevelManager.GameMode.SinglePlayerChallange);
+	}
+
+	void StartSinglePlayerFreePlay(){
+		LevelManager.manager.StartNewGame(LevelManager.GameMode.SinglePlayerFreeplay);
+	}
+
+}
